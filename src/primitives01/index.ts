2aa32131728f5a316c9f39a2632b9b33d3405121
kimdonggyu <dkkim1004@hanmail.net>
2024-09-01 16:06:47 +0900

    타입스크립트 설정

---
+++ src/primitives01/index.ts
@@ -0,0 +1,21 @@
+import * as THREE from "three";
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+const scene = new THREE.Scene();
+scene.add(cube);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+document.body.appendChild(renderer.domElement);
